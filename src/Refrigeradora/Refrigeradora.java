85de38aa0616550b1a8ae0305c46b33cc99d208d
SUPERFARMACIA <SUPERFARMACIA@farmacia>
2023-05-21 12:37:09 -0500

    primer commit

---
+++ src/Refrigeradora/Refrigeradora.java
@@ -0,0 +1,72 @@
+
+package Refrigeradora;
+
+/**
+ *
+ * @author SUPERFARMACIA
+ */
+public class Refrigeradora {
+    private String nombre;
+    private String repisas;
+    private String recipiente;
+    private String congelador;
+    private String marca;
+    private String motor;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRepisas() {
+        return repisas;
+    }
+
+    public void setRepisas(String repisas) {
+        this.repisas = repisas;
+    }
+
+    public String getRecipiente() {
+        return recipiente;
+    }
+
+    public void setRecipiente(String recipiente) {
+        this.recipiente = recipiente;
+    }
+
+    public String getCongelador() {
+        return congelador;
+    }
+
+    public void setCongelador(String congelador) {
+        this.congelador = congelador;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+            
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
